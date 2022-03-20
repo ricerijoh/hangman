f5d5585c5b57fd2f4aae4ed1a6bdbd162e61a658
Richard Johansson <ricerijoh@gmail.com>
2022-03-20 12:48:29 +0100

    script is finished and readme is updated

--- README.md
+++ README.md
@@ -2,11 +2,10 @@
 ## Description:
 This application is written in Python where a json file is parsed and a random word is chosen from the json. The user then guesses letters that the word contains and if the user can't guess the word within n-number of times will the user loose.
 
-## Code
+## Code explanation
 Create a class called `Hangman()`. in `__init__` read the json file with the words and initiate serveral things including a random integer generator which choses a word at random from the json file with words in the method `get_rnd_word()`. 
+ 
+Method `print_hidden_word()` prints out the attribute `.hidden_word` which is initialized from the json list of words together with the method `get_rnd_word()` and the `hidden_word()` method. The method `hidden_word()` uses the attribute `.split` and loops all characters from the word into a list attribute using listcomprension.
 
-Create a method `print_hidden_word()` which prints out something like the following:
-```
-The word is: __
-```
-for the word `is` 
+The method `update_hidden_word()` takes an input argument. This input is then checked if the character is in the random word that has been generated. If the character exists in the word will the index of where it is be saved and then looped into `.hidden_word` in the correct place, this also works for more than one occurance of the character. `update_hidden_word()` also calls the method `check_word()` after `.hidden_word` has been updated. In the `check_word()` and `check_count()` methods they check if the attribute `.hidden_word` is equal to the attribute `.word` and that the count limit not has been reached.
+
--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import pandas as pd
 
@@ -6,25 +7,58 @@ class Hangman():
         self.json = pd.read_json("data/word.json")
         self.rnd = np.random.randint(low = 0, high = 99)
         self.word = pd.DataFrame()
-        self.correct_word = list()
+        self.count = 0
 
     def get_rnd_word(self):
         self.word = self.json["word"].iloc[self.rnd]
         self.split = [char for char in self.word]
     
     def hidden_word(self):
-        self.hidden_word = "_ "*len(self.split)
+        self.hidden_word = "_"*len(self.split)
+        self.hidden_word = [char for char in self.hidden_word]
 
     def print_hidden_word(self):
-        print("The word is: " + self.hidden_word)
-        
+        print("The word is: ", "".join(self.hidden_word))
+    
+    def update_hidden_word(self, inp):
+        inp = inp.lower()
+        if len(inp) == 1:
+            if inp in self.word:
+                self.idx = [_.start() for _ in re.finditer(inp, self.word)]
+                if len(self.idx) > 1:
+                    for idx in self.idx:
+                        self.hidden_word[idx] = inp
+                    self.print_hidden_word()
+                else:
+                    self.hidden_word[self.idx[0]] = inp
+                    self.print_hidden_word()
+            else:
+                self.count = self.count + 1
+        else:
+            print("Guess one character a-z")
 
+        if inp.lower() not in self.word:
+            print(f"{inp} is not in the word")
+        self.check_word()
+        self.check_count()
 
-        
+    def check_word(self):
+        if "".join(self.hidden_word) == self.word:
+            print("YOU WON!")
+    def check_count(self):
+        if self.count == 5:
+            print("YOU LOST! the word was: ", self.word)
 
-
+print("\n")
+print("This is Hang-man!")
+print("You are supposed to guess the letter in the word")
+print("if you can't make it will the man hang! :)\n")
 hangman = Hangman()
 hangman.get_rnd_word()
 hangman.hidden_word()
 hangman.print_hidden_word()
 
+while hangman.count < 5 and "".join(hangman.hidden_word) != hangman.word:
+    inp = input()
+    hangman.update_hidden_word(inp)
+    print("\n")
